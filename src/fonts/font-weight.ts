5177fd83e508a13b11f44327fdf46d80798ab78c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-10-05 17:53:46 +0300

    create enum for font weight variants

---
+++ src/fonts/font-weight.ts
@@ -0,0 +1,11 @@
+export enum FontWeight {
+  Ultralight = '100',
+  Thin = '200',
+  Light = '300',
+  Regular = '400',
+  Medium = '500',
+  Semibold = '600',
+  Bold = '700',
+  Heavy = '800',
+  Black = '900',
+}
