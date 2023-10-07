2a91fe1a44c217e34e2ff9ee89bdfd70f2cb7988
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-10-07 15:50:04 +0300

    init Metrika script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 
 import { AppConstants } from '@/app/app.constants';
 import Footer from '@/src/components/layout/Footer/Footer';
@@ -33,6 +34,20 @@ export default function RootLayout({
 
           <Footer />
         </Providers>
+
+        <Script id={'metrika-counter'} strategy={'afterInteractive'}>
+          {`   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+           m[i].l=1*new Date();
+           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        
+           ym(95183872, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+           });`}
+        </Script>
       </body>
     </html>
   );
