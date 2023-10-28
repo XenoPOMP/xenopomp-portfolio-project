d899e762f5edbdcc7e33032aa37a481646a11bee
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-10-28 20:23:26 +0300

    change OG image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import Script from 'next/script';
 
 import { AppConstants } from '@/app/app.constants';
-import ogImage from '@/public/arts/OG Image placeholder.webp';
+import ogImage from '@/public/arts/OG Image.png';
 import Footer from '@/src/components/layout/Footer/Footer';
 import Header from '@/src/components/layout/Header/Header';
 import Providers from '@/src/components/layout/Providers/Providers';
