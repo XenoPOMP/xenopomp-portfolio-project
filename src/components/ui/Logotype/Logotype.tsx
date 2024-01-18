0f3433f944891fcca3deff4d53d3d017b4f2185a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-01-18 10:51:25 +0300

    change font of logo

--- src/components/ui/Logotype/Logotype.tsx
+++ src/components/ui/Logotype/Logotype.tsx
@@ -1,13 +1,19 @@
 import cn from 'classnames';
+import { Unbounded } from 'next/font/google';
 import Link from 'next/link';
 import { FC } from 'react';
 
 import styles from './Logotype.module.scss';
 import type { LogotypeProps } from './Logotype.props';
 
+const logoFont = Unbounded({ subsets: ['latin', 'cyrillic'] });
+
 const Logotype: FC<LogotypeProps> = ({ className, ...props }) => {
   return (
-    <Link href={'/#'} className={cn('font-bold', styles.logo, className)}>
+    <Link
+      href={'/#'}
+      className={cn('font-bold', styles.logo, logoFont.className, className)}
+    >
       xeno.pomp
     </Link>
   );
