c3ca5144ff9b64584f44655b26bde2342084c286
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-01-20 10:43:03 +0300

    create normal project percent calculation

--- src/sections/PercentageSection/PercentageSection.tsx
+++ src/sections/PercentageSection/PercentageSection.tsx
@@ -1,13 +1,17 @@
-import { Defined, RecordKey, VariableFC } from '@xenopomp/advanced-types';
+import { VariableFC } from '@xenopomp/advanced-types';
 import { getObjectKeys } from '@xenopomp/advanced-utils';
 import cn from 'classnames';
-import { ComponentProps, FC } from 'react';
+import { ComponentProps } from 'react';
 
 import Heading from '@/src/components/ui/Heading/Heading';
 import StackPercentBlock from '@/src/components/ui/StackPercentBlock/StackPercentBlock';
 import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
 import projectData from '@/src/data/project-data';
-import { FullstackTechnology } from '@/src/interfaces/IProject';
+import {
+  BackendStackTechnology,
+  FullstackTechnology,
+  StackTechnology,
+} from '@/src/interfaces/IProject';
 
 import styles from './PercentageSection.module.scss';
 import type { PercentageSectionProps } from './PercentageSection.props';
@@ -21,14 +25,66 @@ const PercentageSection: VariableFC<
     'frontend' | 'backend',
     ComponentProps<typeof StackPercentBlock>['entries']
   > => {
-    const frontend: ComponentProps<typeof StackPercentBlock>['entries'] = [];
-    const backend: ComponentProps<typeof StackPercentBlock>['entries'] = [];
+    const frontEndEntries: Partial<Record<StackTechnology, number>> = {};
+    const backEndEntries: Partial<Record<BackendStackTechnology, number>> = {};
 
-    // TODO: Create normal project stack counter
+    projectData.forEach(({ madeOn, backendStack }) => {
+      if (madeOn !== undefined) {
+        getObjectKeys(madeOn).forEach(stackName => {
+          const isStackUsed = !!madeOn[stackName];
+
+          if (!isStackUsed) {
+            return;
+          }
+
+          if (frontEndEntries[stackName] === undefined) {
+            frontEndEntries[stackName] = 1;
+            return;
+          }
+
+          frontEndEntries[stackName]!++;
+        });
+      }
+
+      if (backendStack !== undefined) {
+        getObjectKeys(backendStack).forEach(backendStackName => {
+          const isStackUsed = !!backendStack[backendStackName];
+
+          if (!isStackUsed) {
+            return;
+          }
+
+          if (backEndEntries[backendStackName] === undefined) {
+            backEndEntries[backendStackName] = 1;
+            return;
+          }
+
+          backEndEntries[backendStackName]!++;
+        });
+      }
+    });
+
+    const traverse = <T extends FullstackTechnology>(
+      obj: Partial<Record<T, number>>,
+      options?: { backendOnly?: boolean }
+    ): ComponentProps<typeof StackPercentBlock>['entries'] => {
+      const keys = getObjectKeys(obj);
+
+      const projects = !!options?.backendOnly
+        ? projectData.filter(project => project.backendStack !== undefined)
+        : projectData;
+
+      return keys.map(key => ({
+        techName: key,
+        percent: (obj[key]! / projects.length) * 100,
+      }));
+    };
 
     return {
-      frontend,
-      backend,
+      frontend: traverse(frontEndEntries),
+      backend: traverse(backEndEntries, {
+        backendOnly: true,
+      }),
     };
   };
 
